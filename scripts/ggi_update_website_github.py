e2a7af7b2e7966def25cbe9b64fbf2448be4f70f
Nicolas Toussaint <nicolas1.toussaint@orange.com>
2025-01-20 20:55:54 +0100

    Update ggi_update_website_github.py

--- scripts/ggi_update_website_github.py
+++ scripts/ggi_update_website_github.py
@@ -74,13 +74,12 @@ def retrieve_env():
             re.sub('^.*/', '', params['GGI_GITHUB_PROJECT']))
         print("- Using public GitHub instance.")
 
-    params['GGI_ACTIVITIES_URL']= urllib.parse.urljoin(params['GGI_GITHUB_URL'], 'issues')
+    params['GGI_ACTIVITIES_URL']= urllib.parse.urljoin(params['GGI_GITHUB_URL'] + '/', 'issues')
     print(f"*** GGI_ACTIVITIES_URL= {params['GGI_ACTIVITIES_URL']}")
     print(f"*** GGI_GITHUB_URL= {params['GGI_GITHUB_URL']}")
     print(f"*** GGI_PAGES_URL= {params['GGI_PAGES_URL']}")
 
     return params
-
 
 def retrieve_github_issues(params: dict):
     print(f"\n# Retrieving project from GitHub at {params['GGI_GITHUB_URL']}.")
